6007f6f5858d5a8fe679642f94391a84c6086f2e
Bienve7742 <bienve.caton@gmail.com>
2025-10-31 18:55:36 +0700

    Finished Project

---
+++ DOM.js
@@ -0,0 +1,10 @@
+document.getElementById("test example").innerHTML = "This is a test using HTML DOM"
+function myFunction() {
+    var a = 7;
+    var b = 6;
+    document.getElementById("buttoncontent").innerHTML = a + b 
+}
+
+function myFunction2() {
+    document.getElementById("buttoncontent2").innerHTML = "Hi! This is extra text from the button."
+}
